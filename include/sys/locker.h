b7458fb72021713a41708ae4c458f02543df6d7c
cxt <cxt1989422@126.com>
2020-05-15 10:47:06 +0800

    fix alpine build error

--- include/sys/locker.h
+++ include/sys/locker.h
@@ -36,7 +36,11 @@ static inline int locker_create(locker_t* locker)
 	// or pthread_cond_wait() may not actually release the mutex (if it had been locked multiple times). 
 	// If this happens, no other thread can satisfy the condition of the predicate. 
 #if defined(OS_LINUX)
-	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
+	#ifdef PTHREAD_MUTEX_RECURSIVE
+		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+	#else
+		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
+	#endif
 	//pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK_NP);
 #else
 	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
